a9957c1efb3191972ac1c0ab9691e35381c80167
Albert Kamalov <KamalovAIu@alfastrah.ru>
2023-10-23 00:12:02 +0200

    Some fix

--- src/main/java/com/example/demo/resource/TaskController.java
+++ src/main/java/com/example/demo/resource/TaskController.java
@@ -5,11 +5,15 @@ import com.example.demo.model.entity.Task;
 import com.example.demo.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/api/task")
@@ -18,8 +22,8 @@ public class TaskController {
     private final TaskService taskService;
 
     @GetMapping(produces = "application/json")
-    public Task getTask() {
-        return new Task(1L, "asd", "asd");
+    public List<Task> getTask() {
+        return List.of(new Task(1L, "asd", "asd"));
     }
 
     @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
